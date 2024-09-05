752c4247e390b4c2811f3a633bbbd0a68e918423
dtarroz <damien.tarroz.apps@gmail.com>
2024-09-05 13:47:15 +0200

    feat: component iml-button

---
+++ src/components/iml-button/iml-button.stories.ts
@@ -0,0 +1,100 @@
+import { makeMeta, makeStory } from '../../stories/lib/make-story.ts';
+import './iml-button.ts';
+
+const meta = makeMeta({
+    tag: 'iml-button',
+    description: 'Surcharge d\'un `<button>` avec différents modes et comportements',
+    slots: [
+        {
+            name: '><',
+            description: 'Le contenu du bouton'
+        }
+    ],
+    attributes: [
+        {
+            name: 'mode',
+            description: 'Le mode de rendu du bouton',
+            control: 'radio',
+            options: ['primary', 'secondary'],
+            type: 'string',
+            defaultValue: 'primary'
+        },
+        {
+            name: 'redirectToUrl',
+            description: 'L\'url de redirection après le clic sur le bouton, seulement si l\'événement n\'a pas été explicitement annulé',
+            control: 'text',
+            type: 'string'
+        },
+        {
+            name: 'disabled',
+            description: 'Le bouton est inactif si la valeur est égal à `true`',
+            control: 'boolean',
+            type: 'boolean'
+        }
+    ],
+    events: [
+        {
+            name: 'iml-button:click',
+            description: 'Evénement sur le clic du bouton'
+        }
+    ],
+    css: [
+        {
+            name: '--iml-button-font-size',
+            description: 'La taille du texte contenu dans le bouton',
+            defaultValue: '0.91rem'
+        }
+    ]
+});
+
+// noinspection JSUnusedGlobalSymbols
+export default { title: 'Atoms/<iml-button>', ...meta };
+
+// noinspection JSUnusedGlobalSymbols
+export const Modes = makeStory({
+    meta: meta,
+    description: 'Différents modes de rendu par défaut du bouton',
+    items: [
+        {
+            '><': 'Primary'
+        },
+        {
+            '><': 'Secondary',
+            'mode': 'secondary'
+        }
+    ]
+});
+
+// noinspection JSUnusedGlobalSymbols
+export const Disabled = makeStory({
+    meta: meta,
+    description: 'Différents modes de rendu du bouton inactif',
+    items: [
+        {
+            '><': 'Primary',
+            disabled: true
+        },
+        {
+            '><': 'Secondary',
+            'mode': 'secondary',
+            disabled: true
+        }
+    ]
+});
+
+// noinspection JSUnusedGlobalSymbols
+export const RedirectToUrl = makeStory({
+    meta: meta,
+    description: 'Redirection vers une url',
+    items: [
+        {
+            '><': 'Avec redirection',
+            redirectToUrl: document.location.origin
+        },
+        {
+            '><': 'Sans redirection',
+            redirectToUrl: document.location.origin,
+            disabled: true
+        }
+    ]
+});
---
+++ src/components/iml-button/iml-button.ts
@@ -0,0 +1,85 @@
+import { ImlHTMLElement } from '../lib/iml-htmlelement.ts';
+import { customElement, property } from '../lib/decorators.ts';
+
+type TypeCustomEventImlButton = 'iml-button:click';
+
+@customElement('iml-button')
+export class ImlButton extends ImlHTMLElement<TypeCustomEventImlButton> {
+
+    /** Le mode de rendu du bouton */
+    @property() mode: 'primary' | 'secondary' = 'primary';
+    
+    /** L'url de redirection après le clic sur le bouton, seulement si l'événement n'a pas été explicitement annulé */
+    @property() redirectToUrl?: string;
+    
+    /** Le bouton est inactif si la valeur est égal à true */
+    @property() disabled: boolean = false;
+
+    override html() {
+        return `<button class="${this.componentClass()}"><slot></slot></button>`;
+    }
+
+    private componentClass() {
+        let classMap: string[] = [this.mode];
+        if (this.disabled)
+            classMap.push('disabled');
+        return classMap.join(' ');
+    }
+
+    override renderUpdated() {
+        this.queryShadowSelector('button, a')?.addEventListener('click', (event) => {
+            if (this.disabled)
+                return;
+            if (!this.dispatchCustomEvent('iml-button:click', { cancelable: true }))
+                event.preventDefault();
+            else if (this.redirectToUrl)
+                document.location = this.redirectToUrl;
+        });
+    }
+
+    override css() {
+        return `
+        <!--suppress CssUnresolvedCustomProperty -->
+        <style>
+            :host {
+                --font-size: var(--iml-button-font-size, 0.91rem);
+                width: 150px;
+            }
+            
+            button {
+                padding: 12px;
+                border-radius: 7px;
+                border: 0;
+                width: 100%;
+                font-family: Arial, sans-serif;
+                font-weight: bold;
+                font-size: var(--font-size);
+                cursor: pointer;
+            }
+            
+            .primary {
+                color: #161616;
+                background-color: hsl(60, 94%, 42%);
+            }
+            
+            .primary:not(.disabled):hover {
+                background-color: hsl(60, 94%, 47%);
+            }
+            
+            .secondary {
+                color:  #c6c6c6;
+                background-color: transparent;
+                border: 1px solid  #c6c6c6;
+            }
+            
+            .secondary:not(.disabled):hover {
+                border: 1px solid hsl(60, 94%, 47%);
+            }
+            
+            .disabled {
+                opacity: 0.5;
+                cursor: default;
+            }
+        </style>`;
+    }
+}
