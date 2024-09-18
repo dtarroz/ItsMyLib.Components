600b171cbc347279b29ec0703d984e8193e610f2
dtarroz <damien.tarroz.apps@gmail.com>
2024-09-18 07:50:27 +0200

    feat(taghelpers): create solution and project

---
+++ src/taghelpers/ItyMyLib.Components.TagHelpers/ImlButtonTagHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ItyMyLib.Components.TagHelpers;
+
+public enum ImlButtonMode
+{
+    // ReSharper disable once InconsistentNaming
+    primary,
+
+    // ReSharper disable once InconsistentNaming
+    secondary
+}
+
+public enum ImlButtonStatus
+{
+    // ReSharper disable once InconsistentNaming
+    active,
+
+    // ReSharper disable once InconsistentNaming
+    inactive,
+
+    // ReSharper disable once InconsistentNaming
+    disabled
+}
+
+[HtmlTargetElement("iml-button")]
+public class ImlButtonTagHelper : TagHelper
+{
+    public ImlButtonMode Mode { get; set; } = ImlButtonMode.primary;
+    public ImlButtonStatus Status { get; set; } = ImlButtonStatus.active;
+    public string? RedirectToUrl { get; set; } = null;
+
+    public override void Process(TagHelperContext context, TagHelperOutput output) {
+        output.Attributes.SetAttribute("mode", Mode.ToString());
+        output.Attributes.SetAttribute("status", Status.ToString());
+        if (!string.IsNullOrEmpty(RedirectToUrl))
+            output.Attributes.SetAttribute("redirect-to-url", RedirectToUrl);
+    }
+}
+
