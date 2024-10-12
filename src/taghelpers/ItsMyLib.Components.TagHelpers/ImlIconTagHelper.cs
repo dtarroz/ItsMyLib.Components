008eac9c844ea513cdb9f8f3e27247767b1174ab
dtarroz <damien.tarroz.apps@gmail.com>
2024-10-12 10:23:18 +0200

    feat(iml-icon taghelper): add iml-icon and iml-icon-arrow taghelper

---
+++ src/taghelpers/ItsMyLib.Components.TagHelpers/ImlIconTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ItsMyLib.Components.TagHelpers;
+
+public enum ImlIconName
+{
+    // ReSharper disable once InconsistentNaming
+    arrow
+}
+
+[HtmlTargetElement("iml-icon")]
+public class ImlIconTagHelper : TagHelper
+{
+    public ImlIconName Name { get; set; } = ImlIconName.arrow;
+
+    public override void Process(TagHelperContext context, TagHelperOutput output) {
+        output.Attributes.SetAttribute("name", Name.ToString());
+    }
+}
