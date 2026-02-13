947855330b384f5668040357a7c0e4c9475a270c
dtarroz <damien.tarroz.apps@gmail.com>
2026-02-14 00:01:50 +0100

    fix(taghelper iml-menu): actions json to camel case

--- src/taghelpers/ItsMyLib.Components.TagHelpers/ImlMenuTagHelper.cs
+++ src/taghelpers/ItsMyLib.Components.TagHelpers/ImlMenuTagHelper.cs
@@ -18,8 +18,12 @@ public class ImlMenuTagHelper : TagHelper
 
     public override void Process(TagHelperContext context, TagHelperOutput output) {
         if (!string.IsNullOrEmpty(Icon))
-            output.Attributes.SetAttribute("icon", Icon.ToString());
+            output.Attributes.SetAttribute("icon", Icon);
         if ((Actions?.Count ?? 0) > 0)
-            output.Attributes.SetAttribute("actions", JsonSerializer.Serialize(Actions));
+            output.Attributes.SetAttribute("actions", JsonSerializer.Serialize(Actions, GetJsonSerializerOptions()));
+    }
+
+    private static JsonSerializerOptions GetJsonSerializerOptions() {
+        return new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
     }
 }
